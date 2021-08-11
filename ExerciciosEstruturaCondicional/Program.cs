c4ca3152831feeccc4d6345008791ee375a78bf9
Eduardo Alves <edu.alves88@outlook.com>
2021-08-11 16:53:44 -0300

    Add project files.

---
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosEstruturaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            Console.WriteLine("Entre com um valor: ");
+            valor = int.Parse(Console.ReadLine());
+            if (valor < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NÃO NEGATIVO");
+            }
+        }
+    }
+}
