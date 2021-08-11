6fcb1ed05d82680cc73dc622b4dc8c5709e525d4
Eduardo Alves <edu.alves88@outlook.com>
2021-08-11 19:19:16 -0300

    Andamento dos exercícios, feito até o terceiro.

--- ExerciciosEstruturaCondicional/Program.cs
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -22,6 +22,44 @@ namespace ExerciciosEstruturaCondicional
                 Console.WriteLine("NÃO NEGATIVO");
             }
 
+            //Fazer um programa para ler um número inteiro
+            //e dizer se este número é par ou ímpar.
+            float parouimpar, resto;
+            Console.WriteLine("Entre com um valor para saber se é par ou ímpar: ");
+            parouimpar = float.Parse(Console.ReadLine());
+            resto = parouimpar%2;    
+            if (resto == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("ÍMPAR");
+            }
+
+            //Leia 2 valores inteiros (A e B).
+            //Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+            //Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+            //ordem crescente ou decrescente.
+            string valIn;
+            Console.WriteLine("Digite os valores a serem conferidos");
+            valIn = Console.ReadLine();
+            string[] vetValores = valIn.Split(' ');
+            int val1 = int.Parse(vetValores[0]);
+            int val2 = int.Parse(vetValores[1]);
+            if (((val1/val2)%2)>0)
+            {
+                Console.WriteLine( "É Divisível!");
+            }
+            else
+            {
+                Console.WriteLine("Não é Divisível!");
+            }
+
+            //Leia a hora inicial e a hora final de um jogo.
+            //A seguir calcule a duração do jogo,
+            //sabendo que o mesmo pode começar em um dia e terminar em outro,
+            //tendo uma duração mínima de 1 hora e máxima de 24 horas.
 
         }
     }
