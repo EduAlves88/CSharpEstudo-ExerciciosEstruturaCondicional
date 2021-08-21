c124275a78931e98a790c3114842377116af9b5d
Eduardo Alves <edu.alves88@outlook.com>
2021-08-20 21:25:30 -0300

    commit final

--- ExerciciosEstruturaCondicional/Program.cs
+++ ExerciciosEstruturaCondicional/Program.cs
@@ -165,19 +165,19 @@ namespace ExerciciosEstruturaCondicional
             float x = float.Parse(parabola[0]);
             float y = float.Parse(parabola[1]);
 
-            if (x==0 && y==0)
+            if (x == 0 && y == 0)
             {
                 Console.WriteLine("Origem");
             }
-            else if (x==0)
+            else if (x == 0)
             {
                 Console.WriteLine("Eixo Y");
             }
-            else if (y==0)
+            else if (y == 0)
             {
                 Console.WriteLine("Eixo X");
             }
-            else if((x > 0) && (y > 0))
+            else if ((x > 0) && (y > 0))
             {
                 //Q1
                 Console.WriteLine("Quadrante Q1");
@@ -201,6 +201,46 @@ namespace ExerciciosEstruturaCondicional
             {
                 Console.WriteLine("Ponto na Origem");
             }
+
+            //Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
+            //que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
+            //qualquer desvio.A moeda deste país é o Rombus, cujo símbolo é o R$.
+            //Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
+            //mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
+            //----------------------------------
+            //0 a 2000 - isento
+            //2000,01 - 3000 -> 8%
+            //3000,01 - 4500 -> 18%
+            //acima de 4500 -> 28%
+            //----------------------------------
+            //Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de 
+            //salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido(abaixo), a taxa é
+            //de 8 % sobre R$ 1000.00 + 18 % sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com
+            Console.WriteLine("Entre com o salário: ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double imposto;
+            if (salario < 2000)
+            {
+                Console.WriteLine("Saída: ISENTO!");
+            }
+            else if (salario < 3000)
+            {
+                imposto = salario * 0.08;
+                Console.WriteLine("Saída: " + imposto);
+            }
+            else if (salario < 4500)
+            {
+                imposto = salario * 0.18;
+                Console.WriteLine("Saída: " + imposto);
+            }
+            else
+            {
+                imposto = salario * 0.28;
+                Console.WriteLine("Saída: " + imposto);
+            }
+
+
+
         }
     }
 }
